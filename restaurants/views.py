c881b8e6af9d3a9d95361bf994f4a547fc32ccea
Yousef Al Othman <Yousef@MacBook-Pro-2.local>
2019-08-20 14:59:56 +0300

    Yousef & Amnah

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,6 +7,12 @@ def restaurant_list(request):
 
     context = {
 
+    "my_list": [
+    {"restaurant_name":"elevation", "food_type":"burger"},
+    {"restaurant_name":"roots", "food_type":"rice"},
+    {"restaurant_name":"mcdonalds", "food_type":"mcchicken",},
+    ],
+
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +20,9 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    "my_object": {
+    "restaurant_name":"almakan", "food_type":"karagi"
+    }
 
     }
     return render(request, 'detail.html', context)
